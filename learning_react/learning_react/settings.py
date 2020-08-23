b39c2f0f440cb0dd4683147e97451cd9f6db0adf
kokosowygosc <mati1995@onet.pl>
2020-08-23 20:17:07 +0200

    Added first simple React App. Compiled it with Babel.

--- learning_react/learning_react/settings.py
+++ learning_react/learning_react/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leads.apps.LeadsConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 if DEBUG != True:
